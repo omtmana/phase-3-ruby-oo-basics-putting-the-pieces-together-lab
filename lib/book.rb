662c4173fadef2e30e22abb808e9e194639647b1
Olivia <oliviatmanalastas@gmail.com>
2022-08-10 18:54:28 -0400

    commit tree 47a9f6d53fe234f8cc63df658a80f46f2fa217e1

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+   attr_reader :title
+   attr_accessor :author, :page_count, :genre 
+   def intialize(title)
+      @title = title
+   end
 
-
+   def turn_page
+      puts "Turn page"
+   end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe
+   attr_reader :brand
+   attr_accessor :color , :size , :material
+   def initialize(brand)
+      @brand = brand
+   end
+
+   def cobble
+      puts "Shoe has been repaired"
+   end
+end
